d60b0c26dabdc5b8d43d3f3ef824769d555fde81
nostazac <110936340+nostazac@users.noreply.github.com>
2023-04-03 12:03:57 +0300

    Add files via upload

---
+++ Musicp/music.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href ="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href ="style.css">
+
+    <title>Domos music</title>
+</head>
+<body>
+    <bgsound src = "Weeknd/The_Weeknd_-_Starboy_ft._Daft_Punk_(Official_Video)(256k).mp3"/>
+
+    <nav class= "navbar navbar-expand-lg navbar-dark bg -dark" id = "mainmenu">
+        <a class="nav-brand" href = "#">Library</a>
+        <button class ="navbar-toggler" type = "button" data-toggle="colapse" id = "#navabarSupportedContent">
+            <span class ="navbar-toggler-icon"></span>
+</button>
+<div class = "collapse navbar-collapse" id ="navbarSupportedContent">
+    <ul class = "navbar-nav mr-auto">
+        <li class = "nav-item active">
+            <a class = "nav-link" href = "#">Home <span class = "sr-only">(current)</span></a>
+
+        </li>
+        <li class =""nav-item>
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class ="nav-item dropdown">
+            <a class= "nav-link dropdown-toggle" href = "#" id = "navbarDropdown" role = "button" data-toggle = "dropdown">Categories 
+            </a>
+        
+        <div class = "dropdown-menu" aria-label1edby = "navbarDropdown" id= "dropdown">
+            <a class = "dropdown-item" href = "#hiphop">Hip-Hop</a>
+            <div class = "dropdown-divider"></div>
+            <a class = "dropdown-item" href =  "#cooljazz">Cool jazz</a>
+            <div class = "dropdown-divider"></div>
+            <a class = "dropdown-item" href = "#reggearoots">Reggea & Roots</a>
+            <div class = "dropdown-divider"></div>
+            <a class = "dropdown-item" href = "#popmusic">Pop music</a>
+            <div class = "dropdown-divider"></div>
+            <a class = "dropdown-item" href = "#smoothr&b">Smooth R&B</a>
+            <div class = "dropdown-divider"></div>
+            <a class = "dropdown-item" href = "#toptrending">Top Trending</a>
+        </div>
+        </li>
+        <li class = "nav-item">
+            <a class = "nav-link disabled" href = "#">Disabled</a>
+        </li>
+    </ul>
+    <form class = "form-inline my-2 my-lg-0">
+
+        <input class = "form-control mr-sm-2" type = "Search" placeholder = "Search" arial-label = "Search">
+        <button class = "btn-outline-success my-2 my-sn-0" type = "submit">Search</button>
+    </form>
+</div>
+</nav>
+<h1>Website for livestreaming music</h1>
+<div class = "bd-example">
+</div >
+<div id = "carouselCaptions" class = "carousel slide" data-ride = "carousel" >
+    <ol class = "carousel indicator">
+        <li data-target = "#carouselCaptions" data-slide-to = "0" class = "active"></li>
+        <li data-target = "#carouselCaptions" data-slide-to = "1"></li>
+        <li data-target = "#carouselCaptions" data-slide-to = "2"></li>
+
+    </ol> 
+<div class = "carousel-inner">
+    <div class = "carousel-item active">
+        <h4>Hip-Hop</h4>
+        <img src="" class = "d-block w -100" alt = "..." width = 80%>
+        <div class = "carousel-caption d-none d-md block">
+        </div>
+    </div>
+    <div class = "carousel-item">
+        <h4>Cool jazz</h4>
+        <img src="" class = "d-block w -100" alt = "..." width = 80%>
+        <div class = "carousel-caption d-none d-md block">
+        </div>
+</div>
+<div class = "carousel-item">
+    <h4>Reggea & Roots</h4>
+        <img src="" class = "d-block w-100" alt = "..." width = 80%>
+        <div class = "carousel-caption d-none d-md block">
+        </div>   
+</div>
+<div class = "carousel-item">
+    <h4>Pop Music</h4>
+        <img src="" class = "d-block w -100" alt = "..." width = 80%>
+        <div class = "carousel-caption d-none d-md block">
+        </div>   
+</div>
+<div class = "carousel-item">
+    <h4>Smooth R&B</h4>
+        <img src="" class = "d-block w -100" alt = "..." width = 80%>
+        <div class = "carousel-caption d-none d-md block">
+        </div>   
+</div>
+<div class = "carousel-item">
+    <h4>Top Trending</h4>
+        <img src="" class = "d-block w -100" alt = "..." width = 80%;>
+        <div class = "carousel-caption d-none d-md block">
+        </div>  
+        <a class = "carousel-control-prev" href = "#carouselCaption" role = "button" dataslide = "Prev">
+            <span class = "carousel-control-prev-icon" aria-hidden ="true"></span>
+            <span class = "sr-only">Previous</span>
+        </a>
+        <a class = "carousel-control-prev" href = "#carouselCaption" role = "button" dataslide = "Next">
+            <span class = "carousel-control-next-icon" aria-hidden ="true"></span>
+            <span class = "sr-only">Next</span>
+         </a> 
+</div>
+
+</div>
+
+<a id =  "hiphop">
+
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Hip-Hop</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+    </div>
+        
+    </section>
+</a>
+<a id =  "cooljazz">
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Cool Jazz</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+    </div>
+        
+    </section>
+</a>
+<a id =  "reggearoots">
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Reggea and roots</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+</div>
+        
+    </section>
+</a>
+<a id =  "popmusic">
+
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Pop Music</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+</div>
+        
+    </section>
+</a>
+<a id =  "smooothr&b">
+
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Smooth R&B</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+    </div>
+        
+    </section>
+</a>
+<a id =  "toptrending">
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Top trending</h2>
+        </div>
+
+        <div class = "container-fluid">
+            <div class = "row">
+                <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/21 Savage Can t Leave Without It Official Audio.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/21 Savage Can t Leave Without It Official Audio.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/A+AP+Rocky+Praise+The+Lord+Da+Shine+Official+Video+ft+Skepta - Copy.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/Juice WRLD Outer Space Lyrics.mp3" class = "img-fluid pb-3">
+                <a href = "Trap/Juice WRLD Outer Space Lyrics.mp3">You can play audio</a></audio>
+        </div>
+        <div class = "col-lg-4 col-md-4 col-12">
+            <audio controls src="Trap/KILLSHOT_[Official_Audio](256k).mp3" class = "img-fluid pb-3">
+                <a href = "Trap/KILLSHOT_[Official_Audio](256k).mp">You can play audio</a></audio>
+        </div>
+        
+</div>
+    </div>
+        
+    </section>
+</a>
+    <section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Login or Signup</h2>
+        </div>
+        <div class = "contactUs">
+        <form action = "music.php" method ="POST" style = "position:center">
+        <br>
+        <div>
+            <label>Email:</label> <input type = "email" name = "email" ><br>
+        </div>
+
+        <div>
+             <label>Username:</label> <input type = "text" name = "username"><br>
+        </div>
+        
+        <div>
+             <label>Password:</label> <input type = "password" name = "password"><br>
+        </div>
+        
+        <input type ="submit" name = "submit">
+    </form>
+
+</div>
+</section>
+<section class = "my-4" style = "align:horizontal">
+        <div class = "py-4">
+        <h2 class = "text-center">Domo</h2>
+        <p>Iam a young developer fond of creating web app and also fond of music</p>
+</div>
+
+</section>
+
+    <?php
+
+    ?>
+    
+</body>
+<footer style ="float: center; padding-top:4em;">
+    <p>Copyright <c> <i>2012</i>All rights reserved</p>
+</footer>
+</html>
